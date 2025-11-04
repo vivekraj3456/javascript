0e2e8dc6bbaf049762e7be5edd86e642aae4828f
vivekraj3456 <vivekraj094@gmail.com>
2025-11-04 19:38:42 +0530

    variable

--- 01_Basic/01_Variable.js
+++ 01_Basic/01_Variable.js
@@ -1,5 +1,21 @@
-const accountId=123456
-let  accountEmail="hitesh@google.com"
-var accountPassword="12345"
-accountCity="jaipur"
+const accountId = 123456;
+let accountEmail = "vivekraj123@gmail.com";
+let accountPassword = "1234567890"; 
+let accountCity = "Jalandhar";
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 console.log(accountId);
+accountEmail="abc123@gmail.com"
+accountPassword =" newpassword123"
+accountCity="New York"
+console.table({ accountEmail, accountPassword, accountCity});
+
+let accountState;
+console.log(accountState); // Output: undefined bcoz variable is declared but not initialized
+
+accountState="Punjab"
+console.log(accountState); // Output: Punjab bcoz variable is now initialized with a value
+
